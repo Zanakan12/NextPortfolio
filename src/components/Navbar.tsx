58c029e6cdd1a9dc4c398c5ce53f55dbbec01fba
Zanakan12 <zanakan12maniac@proton.me>
2025-05-19 14:59:09 +0200

    change sevice name to skill

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav className="hidden md:flex gap-8 items-center text-sm font-bold text-white uppercase">
             <Link href="#project-slider">Project</Link>
-            <Link href="#skills">Services</Link>
+            <Link href="#skills">SKILLS</Link>
             <Link href="#service">Services</Link>
             <Link href="#about">COMPETENCES</Link>
             <Link href="#contact">Contact ME</Link>
